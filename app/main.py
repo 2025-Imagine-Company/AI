882e5e76dfa88626c5b77676fe9525f04aa5ddfe
ìv2n03 <shgudwns1515@naver.com>
2025-10-12 18:35:23 +0900

    modifying

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from dotenv import load_dotenv, find_dotenv
 from fastapi import FastAPI
 from .routers import train
 
-# .env 파일을 자동으로 찾아 로드 (프로젝트 루트 어디에 있든 상관없이)
+from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv(), override=True)
+
+from app.core.config import Settings
+settings = Settings()
 
 def create_app():
     app = FastAPI(title="AudIon AI Server", version="0.1.0")
